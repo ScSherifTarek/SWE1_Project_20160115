8d1400f49189466c8653bb0122f4a0ae15210a37
ScSherifTarek <sherift1552@gmail.com>
2018-12-22 17:49:25 +0200

    developing Option_DB_Interface

--- SWE_Project/src/Item_DB_Interface.java
+++ SWE_Project/src/Item_DB_Interface.java
@@ -3,7 +3,7 @@ public class Item_DB_Interface {
 	private static String tableName= "items";
 	private static String relationWithPostsName = "formsinposts";
 	private static String relationWithQuestionsName = "questionsforitems";
-
+	
 	public static Form getItemById(int id)
 	{
 		return null;
